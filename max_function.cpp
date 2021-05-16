c3e3442464374c0222e71c4028b3f4e1d58946d3
Mohamed Arafaath <mohamed_arafaath@outlook.com>
2021-05-16 09:20:03 +0530

    Create max_function.cpp

---
+++ max_function.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int max(int a, int b, int c)
+{
+        if(a>b)
+            return a;
+        else if(b>c)
+            return b;
+        else
+            return c;
+}
+
+int main()
+{
+   int a= 4, b = 5, c =8;
+   int result = max(a, b, c);
+   cout <<result<<" is the max!";
+    return 0;
+}
